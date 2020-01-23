fc694e95ec74b476c9b6e2e12bb42e12dea89a2d
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-01-23 13:00:34 +0100

    added my first cypress test

---
+++ cypress/integration/userCanCheckTheirBMI.feature.js
@@ -0,0 +1,7 @@
+// userCanCheckTheirBMI.feature.js
+
+describe('User can check their BMI', () => {
+    it('user can ener eight and height', () => {
+        cy.visit('http://localhost:3001')
+    })
+})
