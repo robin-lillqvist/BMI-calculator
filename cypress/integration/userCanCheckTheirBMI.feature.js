8652a05b2614e740a5e7b999fa12345e68278b1b
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-01-23 13:24:37 +0100

    added new test for underweight person

--- cypress/integration/userCanCheckTheirBMI.feature.js
+++ cypress/integration/userCanCheckTheirBMI.feature.js
@@ -13,7 +13,7 @@ describe('Normal weight person check their BMI', () => {
         cy.get('#results').should('contain', '23.04')    
     })
 
-    it('It displays correct classification for values w:95 h:184', () => {
+    it('It displays correct classification for values w:78 h:184', () => {
         cy.get('#results').should('contain', 'Normal weight')    
     })
 })
@@ -33,5 +33,23 @@ describe('Overweight person check their BMI', () => {
 
     it('It displays correct classification for values w:95 h:184', () => {
         cy.get('#results').should('contain', 'Overweight')    
+    })
+})
+
+describe('Underweight person check their BMI', () => {
+    it('user can ener eight and height', () => {
+        cy.visit('http://localhost:3001')
+        cy.contains('BMI Calculator')
+        cy.get('#weight').type('60')
+        cy.get('#height').type('184')
+        cy.get('#Calculate').click()
+    })
+
+    it('It displays correct BMI for values w:60 h:184', () => {
+        cy.get('#results').should('contain', '17.72')    
+    })
+
+    it('It displays correct classification for values w:60 h:184', () => {
+        cy.get('#results').should('contain', 'Normal weight')    
     })
 })
