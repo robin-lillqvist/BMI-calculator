ac6e3a92df9e654d9991b41afb9e82cf82952e34
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-01-23 13:10:08 +0100

    added test to enter values in form and click button

--- cypress/integration/userCanCheckTheirBMI.feature.js
+++ cypress/integration/userCanCheckTheirBMI.feature.js
@@ -3,5 +3,9 @@
 describe('User can check their BMI', () => {
     it('user can ener eight and height', () => {
         cy.visit('http://localhost:3001')
+        cy.contains('BMI Calculator')
+        cy.get('#weight').type('95')
+        cy.get('#height').type('184')
+        cy.get(#Calculate).click()
     })
 })
